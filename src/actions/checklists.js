2fac74360401d2755922e1cae79aa505015139eb
zalkar-z <zak@packet.com>
2020-11-07 16:44:35 -0500

    add todo

--- src/actions/checklists.js
+++ src/actions/checklists.js
@@ -2,5 +2,5 @@ export function setActiveChecklist(store, checklistId) {
   let lists = JSON.parse(localStorage.getItem('lists'));
   if (lists == null) lists = [];
 
-  const activeChecklist = lists.filter(list => )
+  // TODO
 }
